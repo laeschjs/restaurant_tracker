3873f6b753bde4f68d6b91b201e37c7f3ceb8c80
Joshua Laesch <joshua.laesch@appfolio.com>
2023-08-18 07:36:48 -0700

    Trying something allow my fly deployments to work
    https://community.fly.io/t/prisma-sqlite-causes-an-out-of-memory-error-on-deploy/11039/4

--- start.sh
+++ start.sh
@@ -6,5 +6,12 @@
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
 set -ex
+fallocate -l 256M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
 npx prisma migrate deploy
+swapoff /swapfile
+rm /swapfile
 npm run start
