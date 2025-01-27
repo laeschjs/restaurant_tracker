8b16c6d0ce1b4d9160511359d9c2b7673c7a230c
Joshua Laesch <joshua.laesch@appfolio.com>
2025-01-27 06:29:28 -0800

    Remove ClientOnly from the daily page

--- app/routes/seventy_five_hard/daily.tsx
+++ app/routes/seventy_five_hard/daily.tsx
@@ -14,7 +14,6 @@ import {
 
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import type { SeventyFiveHardDailyEntry } from "@prisma/client";
-import { ClientOnly } from "remix-utils";
 
 export async function loader({ request }: LoaderArgs) {
   const userId = await requireUserId(request);
@@ -53,109 +52,99 @@ export default function Daily() {
   // TODO: either add a message or redirect if they go to this route and the current date is not in the challenge
 
   return (
-    <ClientOnly>
-      {() => (
-        <Sheet
-          color="neutral"
-          variant="outlined"
-          className="m-5 rounded-md p-5 text-center sm:mx-auto"
+    <Sheet
+      color="neutral"
+      variant="outlined"
+      className="m-5 rounded-md p-5 text-center sm:mx-auto"
+    >
+      {actionData && actionData.success && (
+        <Alert
+          color="success"
+          variant="solid"
+          className="my-5"
+          startDecorator={<FontAwesomeIcon icon={faCircleCheck} size="sm" />}
         >
-          {actionData && actionData.success && (
-            <Alert
-              color="success"
-              variant="solid"
-              className="my-5"
-              startDecorator={
-                <FontAwesomeIcon icon={faCircleCheck} size="sm" />
-              }
-            >
-              {actionData.message}
-            </Alert>
-          )}
-          <Form method="post">
-            <input type="hidden" name="entryId" value={entry?.id} />
-            <input type="hidden" name="challengeId" value={challenge.id} />
-            <input
-              type="hidden"
-              name="date"
-              value={new Date().toDateString()}
-            />
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Weight:</span>
-              <TextField name="weight" defaultValue={entry?.weight} />
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Drank 1 Gal Water:</span>
-              <div>
-                No
-                <Switch
-                  name="drankWater"
-                  defaultChecked={entry?.drankWater ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Indoor Workout:</span>
-              <div>
-                No
-                <Switch
-                  name="indoorWorkout"
-                  defaultChecked={entry?.indoorWorkout ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Outdoor Workout:</span>
-              <div>
-                No
-                <Switch
-                  name="outdoorWorkout"
-                  defaultChecked={entry?.outdoorWorkout ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Read 10 Pages:</span>
-              <div>
-                No
-                <Switch
-                  name="readTenPages"
-                  defaultChecked={entry?.readTenPages ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Follow Diet:</span>
-              <div>
-                No
-                <Switch
-                  name="followedDiet"
-                  defaultChecked={entry?.followedDiet ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
-              <span>Progress Pic:</span>
-              <div>
-                No
-                <Switch
-                  name="imageTaken"
-                  defaultChecked={entry?.imageTaken ?? false}
-                />
-                Yes
-              </div>
-            </label>
-            <Button type="submit" color="success" size="lg" className="block">
-              Update
-            </Button>
-          </Form>
-        </Sheet>
+          {actionData.message}
+        </Alert>
       )}
-    </ClientOnly>
+      <Form method="post">
+        <input type="hidden" name="entryId" value={entry?.id} />
+        <input type="hidden" name="challengeId" value={challenge.id} />
+        <input type="hidden" name="date" value={new Date().toDateString()} />
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Weight:</span>
+          <TextField name="weight" defaultValue={entry?.weight} />
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Drank 1 Gal Water:</span>
+          <div>
+            No
+            <Switch
+              name="drankWater"
+              defaultChecked={entry?.drankWater ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Indoor Workout:</span>
+          <div>
+            No
+            <Switch
+              name="indoorWorkout"
+              defaultChecked={entry?.indoorWorkout ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Outdoor Workout:</span>
+          <div>
+            No
+            <Switch
+              name="outdoorWorkout"
+              defaultChecked={entry?.outdoorWorkout ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Read 10 Pages:</span>
+          <div>
+            No
+            <Switch
+              name="readTenPages"
+              defaultChecked={entry?.readTenPages ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Follow Diet:</span>
+          <div>
+            No
+            <Switch
+              name="followedDiet"
+              defaultChecked={entry?.followedDiet ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <label className="my-3 flex grid grid-cols-2 items-center justify-items-start gap-1">
+          <span>Progress Pic:</span>
+          <div>
+            No
+            <Switch
+              name="imageTaken"
+              defaultChecked={entry?.imageTaken ?? false}
+            />
+            Yes
+          </div>
+        </label>
+        <Button type="submit" color="success" size="lg" className="block">
+          Update
+        </Button>
+      </Form>
+    </Sheet>
   );
 }
